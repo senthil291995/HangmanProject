23aa30accb0d9abad3db3ca400a9a4dc37f29a43
Abishek-Amstrong <abishekamstrong@gmail.com>
2020-04-04 19:50:19 +0530

    added select levels page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SelectLevelComponent } from './components/select-level/select-level.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'select-level', component: SelectLevelComponent },
+  {
+    path: '',
+    redirectTo: '/select-level',
+    pathMatch: 'full'
+  },
+  { path: '**', component: SelectLevelComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/select-level/select-level.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './select-level.component.html',
+  styleUrls: ['./select-level.component.scss']
+})
+export class SelectLevelComponent {
+
+}
